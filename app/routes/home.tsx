a815fc5d5beb1328293e50b13287dd4432ca6f52
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-28 09:13:09 +0600

    mock resume data test

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,30 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumeasy" },
+    { name: "description", content: "Feedback you need!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Resume ratings and Applications</h1>
+          <h2>Review your submissions and get private AI powered feedback</h2>
+        </div>
+      </section>
+      {resumes.map((resume: any) => (
+        <div>
+          <h1>{resume.jobTitle}</h1>
+        </div>
+      ))}
+    </main>
+  );
 }
