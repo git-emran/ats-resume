3aac099a585944188cad7bb3e3ea942fc9a61380
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-08-01 13:50:19 +0600

    Scorecard in the front page added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
 import { Link } from 'react-router'
 
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-        <Link to="/">
-        <p className='text-2xl font-bold text-gradient'>RESUMEASY</p>
-        </Link>
-        <Link to="/" className='primary-button w-fit'>
-        Upload Resume
-        </Link>
-    </nav>
-  )
+    return (
+        <nav className='navbar'>
+            <Link to="/">
+                <p className='text-2xl font-bold text-gradient'>RESUMEASY</p>
+            </Link>
+            <Link to="/" className='primary-button w-fit'>
+                Upload Resume
+            </Link>
+        </nav>
+    )
 }
 
 export default Navbar
