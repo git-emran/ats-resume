a5dde0e65e7c3a16c1ab6178b645e70d2d4b6bab
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-08-06 16:38:48 +0600

    puter authentication added

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -12,7 +12,7 @@ const ResumeCard = ({
             to={`/resume/${id}`}
             className="resume-card animate-in fade-in duration-1000"
         >
-            <div className="resume-card-header">
+            <div className="resume-card-header gap-5">
                 <div className="flex flex-col gap-2">
                     <h2 className="!text-black font-bold break-words">{companyName}</h2>
                     <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,10 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 import Navbar from "~/components/Navbar";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 
 export function meta({ }: Route.MetaArgs) {
@@ -13,6 +15,13 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!auth.isAuthenticated) navigate("/auth?next=/")
+    }, [auth.isAuthenticated])
+
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
             <Navbar />
